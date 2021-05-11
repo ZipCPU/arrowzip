ee92e7fa4f910f33747573c8fbe88fbe402b52f7
ZipCPU <dgisselq@ieee.org>
2021-05-11 08:07:40 -0400

    SDRAMSIM: Fixed (missing) byte-mask handling in first write word

--- sim/verilated/sdramsim.cpp
+++ sim/verilated/sdramsim.cpp
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 //
 // Filename: 	sdramsim.cpp
-//
+// {{{
 // Project:	ArrowZip, a demonstration of the Arrow MAX1000 FPGA board
 //
 // Purpose:	This module simulates an 16-bit SDRAM within a Verilator based
@@ -16,9 +16,9 @@
 //		Gisselquist Technology, LLC
 //
 ////////////////////////////////////////////////////////////////////////////////
-//
-// Copyright (C) 2018, Gisselquist Technology, LLC
-//
+// }}}
+// Copyright (C) 2018-2021, Gisselquist Technology, LLC
+// {{{
 // This program is free software (firmware): you can redistribute it and/or
 // modify it under the terms of  the GNU General Public License as published
 // by the Free Software Foundation, either version 3 of the License, or (at
@@ -33,14 +33,14 @@
 // with this program.  (It's in the $(ROOT)/doc directory.  Run make with no
 // target there if the PDF file isn't present.)  If not, see
 // <http://www.gnu.org/licenses/> for a copy.
-//
+// }}}
 // License:	GPL, v3, as defined and found on www.gnu.org,
+// {{{
 //		http://www.gnu.org/licenses/gpl.html
-//
 //
 ////////////////////////////////////////////////////////////////////////////////
 //
-//
+// }}}
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -57,32 +57,40 @@ short	SDRAMSIM::operator()(int clk, int cke, int cs_n, int ras_n, int cas_n, int
 		return m_last_value; // Always called w/clk=1, thus never here
 	if (!cke) {
 		fprintf(stderr, "SDRAM: This SDRAM simulation only supports CKE high!\n");
+		// {{{
 		fprintf(stderr, "\tCKE   = %d\n", cke);
 		fprintf(stderr, "\tCS_n  = %d\n", cs_n);
 		fprintf(stderr, "\tRAS_n = %d\n", ras_n);
 		fprintf(stderr, "\tCAS_n = %d\n", cas_n);
 		fprintf(stderr, "\tWE_n  = %d\n", we_n);
 		assert(cke);
+		// }}}
 	}
 
 	if (m_pwrup < POWERED_UP_STATE) {
+		// {{{
 		assert(dqm == 3);
 		if (m_clocks_till_idle > 0)
 			m_clocks_till_idle--;
 		if (m_pwrup == 0) {
+			// {{{
 			assert((cs_n)&&(ras_n)&&(cas_n)&&(we_n)&&(dqm==3));
 			if (m_clocks_till_idle == 0) {
 				m_pwrup++;
 				if (m_debug) printf("SDRAM: Successful power up wait, moving to state #1\n");
 			}
+			// }}}
 		} else if (m_pwrup == 1) {
+			// {{{
 			if ((!cs_n)&&(!ras_n)&&(cas_n)&&(!we_n)&&(addr&0x0400)) {
 				// Wait until a precharge all banks command
 				m_pwrup++;
 				if (m_debug) printf("SDRAM: Successful precharge command, moving to state #2\n");
 				m_clocks_till_idle = 8;
 			}
+			// }}}
 		} else if (m_pwrup == 2) {
+			// {{{
 			static	bool	mode_is_set = false;
 			static	int	refresh_cycles = 0;
 
@@ -111,7 +119,9 @@ short	SDRAMSIM::operator()(int clk, int cke, int cs_n, int ras_n, int cas_n, int
 
 				if (m_debug) printf("SDRAM: Moving to power up state 3\n");
 			}
+			// }}}
 		} else if (m_pwrup == 3) {
+			// {{{
 			assert(cs_n);
 			if (m_clocks_till_idle == 0) {
 				m_pwrup = POWERED_UP_STATE;
@@ -120,7 +130,9 @@ short	SDRAMSIM::operator()(int clk, int cke, int cs_n, int ras_n, int cas_n, int
 			} else if (m_clocks_till_idle == 1) {
 				;
 			} else assert(0 && "SDRAM: Should never get here!");
+			// }}}
 		} else if (m_pwrup == 4) {
+			// {{{
 			if ((!cs_n)&&(!ras_n)&&(!cas_n)&&(we_n)) {
 				if (m_clocks_till_idle == 0) {
 					m_pwrup = POWERED_UP_STATE;
@@ -132,8 +144,10 @@ short	SDRAMSIM::operator()(int clk, int cke, int cs_n, int ras_n, int cas_n, int
 			} else {
 				assert(0);
 			}
+			// }}}
 		}
 		m_next_wr = false;
+		// }}}
 	} else { // In operation ...
 		for(int i=0; i<m_nrefresh; i++)
 			m_refresh_time[i]--;
@@ -163,6 +177,8 @@ short	SDRAMSIM::operator()(int clk, int cke, int cs_n, int ras_n, int cas_n, int
 		}
 
 		if ((m_clocks_till_idle > 0)&&(m_next_wr)) {
+			// Second half of a write
+			// {{{
 			if (m_debug) printf("SDRAM[%08x] <= %04x (BM=%d)\n", m_wr_addr, data & 0x0ffff, (dqm&3)^3);
 			int	waddr = m_wr_addr++, memval;
 			memval = m_mem[waddr];
@@ -177,6 +193,7 @@ short	SDRAMSIM::operator()(int clk, int cke, int cs_n, int ras_n, int cas_n, int
 			m_mem[waddr] = memval;
 			result = data;
 			m_next_wr = false;
+			// }}}
 		}
 		m_qloc = (m_qloc + 1)&m_qmask;
 		result = (driv)?data:m_qdata[(m_qloc)&m_qmask];
@@ -187,6 +204,7 @@ short	SDRAMSIM::operator()(int clk, int cke, int cs_n, int ras_n, int cas_n, int
 
 		if ((!cs_n)&&(!ras_n)&&(!cas_n)&&(we_n)) {
 			// Auto-refresh command
+			// {{{
 			m_refresh_time[m_refresh_loc] = MAX_REFRESH_TIME;
 			m_refresh_loc++;
 			if (m_refresh_loc >= m_nrefresh)
@@ -195,21 +213,27 @@ short	SDRAMSIM::operator()(int clk, int cke, int cs_n, int ras_n, int cas_n, int
 			assert((m_bank_status[1]&6) == 0);
 			assert((m_bank_status[2]&6) == 0);
 			assert((m_bank_status[3]&6) == 0);
+			// }}}
 		} else if ((!cs_n)&&(!ras_n)&&(cas_n)&&(!we_n)) {
 			if (addr&0x0400) {
 				// Bank/Precharge All CMD
+				// {{{
 				for(int i=0; i<NBANKS; i++)
 					m_bank_status[i] &= 0x03;
+				// }}}
 			} else {
 				// Precharge/close single bank
+				// {{{
 				assert(0 == (bs & (~3))); // Assert w/in bounds
 				m_bank_status[bs] &= 0x03; // Close the bank
 
 				if (m_debug) printf("SDRAM: Precharging bank %d\n", bs);
+				// }}}
 			}
 		} else if ((!cs_n)&&(!ras_n)&&(cas_n)&&(we_n)) {
-			if (m_debug) printf("SDRAM: Activating bank %d\n", bs);
 			// Activate a bank!
+			// {{{
+			if (m_debug) printf("SDRAM: Activating bank %d\n", bs);
 			if (0 != (bs & (~3))) {
 				m_fail = 2;
 				fprintf(stderr, "ERR: Activating a bank w/ more than 2 bits\n");
@@ -223,10 +247,13 @@ short	SDRAMSIM::operator()(int clk, int cke, int cs_n, int ras_n, int cas_n, int
 			m_bank_status[bs] |= 4;
 			m_bank_open_time[bs] = MAX_BANKOPEN_TIME;
 			m_bank_row[bs] = addr;
+			// }}}
 		} else if ((!cs_n)&&(ras_n)&&(!cas_n)) {
 			if (m_debug) printf("SDRAM: R/W Op\n");
 			if (!we_n) {
 				// Initiate a write
+				// {{{
+				int	waddr, memval;
 				assert(0 == (bs & (~3))); // Assert w/in bounds
 				assert(m_bank_status[bs]&1); // Assert bank is open
 
@@ -235,10 +262,20 @@ short	SDRAMSIM::operator()(int clk, int cke, int cs_n, int ras_n, int cas_n, int
 				m_wr_addr |= bs;
 				m_wr_addr <<= 9;
 				m_wr_addr |= (addr & 0x01ff);
+				waddr = m_wr_addr++;
 
 				assert(driv);
 				if (m_debug) printf("SDRAM: SDRAM[%08x] <= %04x\n", m_wr_addr, data & 0x0ffff);
-				m_mem[m_wr_addr++] = data;
+				memval = m_mem[waddr];
+				if ((dqm&3)==0)
+					memval = data;
+				else if ((dqm&3)==3)
+					;
+				else if ((dqm&2)==0)
+					memval = (memval & 0x000ff) | (data & 0x0ff00);
+				else // if ((dqm&1)==0)
+					memval = (memval & 0x0ff00) | (data & 0x000ff);
+				m_mem[waddr] = data;
 				m_clocks_till_idle = 2;
 				m_next_wr = true;
 
@@ -246,7 +283,9 @@ short	SDRAMSIM::operator()(int clk, int cke, int cs_n, int ras_n, int cas_n, int
 					m_bank_status[bs] &= 3;
 					m_bank_open_time[bs] = MAX_BANKOPEN_TIME;
 				}
+				// }}}
 			} else { // Initiate a read
+				// {{{
 				assert(0 == (bs & (~3))); // Assert w/in bounds
 				assert(m_bank_status[bs]&1); // Assert bank is open
 
@@ -273,6 +312,7 @@ short	SDRAMSIM::operator()(int clk, int cke, int cs_n, int ras_n, int cas_n, int
 					m_bank_status[bs] &= 3;
 					m_bank_open_time[bs] = MAX_BANKOPEN_TIME;
 				}
+				// }}}
 			}
 		} else if (cs_n) {
 			// Chips not asserted, DESELECT CMD equivalent of a NOOP
@@ -280,11 +320,13 @@ short	SDRAMSIM::operator()(int clk, int cke, int cs_n, int ras_n, int cas_n, int
 			// NOOP command
 		} else {
 			fprintf(stderr, "Unrecognized memory command!\n");
+			// {{{
 			fprintf(stderr, "\tCS_n  = %d\n", cs_n);
 			fprintf(stderr, "\tRAS_n = %d\n", ras_n);
 			fprintf(stderr, "\tCAS_n = %d\n", cas_n);
 			fprintf(stderr, "\tWE_n  = %d\n", we_n);
 			assert(0 && "Unrecognizned command");
+			// }}}
 		}
 	}
 
